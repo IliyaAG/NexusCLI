6a510ad81e713f210a19ca97192ce9a99eaae963
IliyaAG <iliyaagh76@gmail.com>
2025-08-19 20:40:44 +0330

    add README.md

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ Command line interface for sonatype nexus repository
 
 # Table of contents:
 
+- [Subcommands:](#subcommands)
+  - [command](#command)
+  - [config](#config)
+  - [user](#user)
+  - [repo](#repo)
+- [Quick Setup:](#quick-setup)
+  - [Build From Source Code](#build-from-source-code)
+  - [Install Package](#install-package)
+  - [Auto Completion](#auto-completion)
+
+# Subcommands:
+## Command
+In this project, there is a subcommand called command, using which you can get the list of available commands and you can add anything you need to the project or write it in the issue section
+
+## config
+
+## user
+
+## repo
+
+# Quick Setup:
+## Build From Source Code
+
+## Install Package
+
+## Auto Completion
--- cmd/command.go
+++ cmd/command.go
@@ -2,39 +2,41 @@ package cmd
 
 import (
     "fmt"
-    "os"
-    "text/tabwriter"
     "github.com/spf13/cobra"
 )
 
 // commandCmd represents the "command" parent command
 var commandCmd = &cobra.Command{
-	Use:   "command",
-	Short: "Manage and inspect available CLI commands",
-	Long:  `The 'command' group helps you to inspect available commands of nexuscli.`,
+    Use:   "command",
+    Short: "Manage and inspect available CLI commands",
+    Long:  `The 'command' group helps you to inspect available commands of nexuscli.`,
 }
 
 // commandListCmd represents the "list" subcommand
 var commandListCmd = &cobra.Command{
     Use:   "list",
-    Short: "List all available commands in nexuscli",
+    Short: "List all available commands in nexuscli (tree view)",
     Run: func(cmd *cobra.Command, args []string) {
-		// Create a tabwriter for pretty output
-        w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
-        fmt.Fprintln(w, "COMMAND\tDESCRIPTION")
-
-		// Get all root subcommands
-        for _, c := range rootCmd.Commands() {
-            fmt.Fprintf(w, "%s\t%s\n", c.Use, c.Short)
-
-			// Print subcommands of each command too
-            for _, sub := range c.Commands() {
-                fmt.Fprintf(w, "  %s %s\t%s\n", c.Use, sub.Use, sub.Short)
-            }
-        }
-
-        w.Flush()
+        fmt.Println("Available Commands:")
+        printCommands(rootCmd, 0)
     },
+}
+
+// Recursive function to print commands with indent
+func printCommands(c *cobra.Command, indent int) {
+    prefix := ""
+    for i := 0; i < indent; i++ {
+        prefix += "  " // two spaces indent
+    }
+
+	// Skip printing the root itself, just its children
+    if c != rootCmd {
+        fmt.Printf("%s- %s : %s\n", prefix, c.Use, c.Short)
+    }
+
+    for _, child := range c.Commands() {
+        printCommands(child, indent+1)
+    }
 }
 
 func init() {
